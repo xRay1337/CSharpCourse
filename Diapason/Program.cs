b8ba8e469cbc009fa82d97808c65c3a0ed39f662
Vitaliy Zlobin <zlovian@yandex.ru>
2020-02-03 20:27:17 +0700

    Add Range

---
+++ Diapason/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Diapason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Range range = new Range(6, -1.5);
+
+            Console.WriteLine("Длина интервала равна {0}", range.GetLength());
+            Console.WriteLine("Число 6.1 входит в инервал: {0}", range.IsInside(6.1));
+            Console.WriteLine("Число 6.0 входит в инервал: {0}", range.IsInside(6.0));
+            Console.WriteLine("Число 5.9 входит в инервал: {0}", range.IsInside(5.9));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Diapason/Range.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Diapason
+{
+    class Range
+    {
+        private double From { get; set; }
+        private double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            if (from < to)
+            {
+                From = from;
+                To = to;
+            }
+            else
+            {
+                From = to;
+                To = from;
+            }
+        }
+
+        public double GetLength()
+        {
+            return Math.Abs(From - To);
+        }
+
+        public bool IsInside(double number)
+        {
+            if (number >= From && number <= To)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
