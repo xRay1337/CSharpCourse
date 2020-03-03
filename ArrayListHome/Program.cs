b3bab73bc9111cf3121a8722dac34619164e3738
Vitaliy Zlobin <zlovian@yandex.ru>
2020-03-04 00:09:25 +0700

    Create ArrayListHome

---
+++ ArrayListHome/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ArrayListHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтение из файла:");
+
+            List<string> textFromFile = ReadFile("..\\..\\text.txt");
+
+            foreach (var e in textFromFile)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Удалить все четные числа:");
+
+            List<int> numbers = new List<int> { 1, 1, 2, 3, 4, 5, 5 };
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    numbers.RemoveAt(i);
+                }
+            }
+
+            foreach (var e in numbers)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Удалить дубли:");
+
+            List<int> uniqueNumbers = RemoveDuplicates(numbers);
+
+            foreach (var e in uniqueNumbers)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<int> RemoveDuplicates(List<int> numbers)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var e in numbers)
+            {
+                if (!result.Contains(e))
+                {
+                    result.Add(e);
+                }
+            }
+
+            return result;
+        }
+
+        static List<string> ReadFile(string path)
+        {
+            List<string> result = new List<string>();
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                string currentString;
+
+                while ((currentString = streamReader.ReadLine()) != null)
+                {
+                    result.Add(currentString);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ArrayListHome/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ArrayListHome")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArrayListHome")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("53e5787c-6647-4031-a17a-951d56ee7930")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
