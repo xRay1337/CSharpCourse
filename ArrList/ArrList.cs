5bab5e8df3169536e848dfd5649ffba012f93aa0
Vitaliy Zlobin <zlovian@yandex.ru>
2020-03-06 23:04:18 +0700

    Add ArrList

---
+++ ArrList/ArrList.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArrList
+{
+    class ArrList<T> : IList<T>
+    {
+        private T[] items = new T[10];
+
+        private int length;
+
+        public int Count
+        {
+            get { return length; }
+        }
+
+        private bool readOnly = false;
+
+        public bool IsReadOnly
+        {
+            get { return readOnly; }
+        }
+
+        public T this[int index]
+        {
+            get { return items[index]; }
+            set { items[index] = value; }
+        }
+
+        public void Add(T item)
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("This array is read only");
+            }
+
+            if (length >= items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            items[length] = item;
+            length++;
+        }
+
+        private void IncreaseCapacity()
+        {
+            T[] old = items;
+            items = new T[old.Length * 2];
+            Array.Copy(old, items, old.Length);
+        }
+
+        public void Clear()
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("This array is read only");
+            }
+
+            items = new T[items.Length];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (int i = 0, j = arrayIndex; i < Count; i++, j++)
+            {
+                array[j] = items[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("This array is read only");
+            }
+
+            if (length + 1 > items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            length++;
+
+            for (int i = length; i > index; i--)
+            {
+                items[i] = items[i - 1];
+            }
+
+            items[index] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("This array is read only");
+            }
+
+            bool flag = false;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    RemoveAt(i);
+                    flag = true;
+                }
+            }
+
+            return flag;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("This array is read only");
+            }
+
+            if (index < 0 || index >= length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (int i = index; i < length - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            length--;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return "[ " + string.Join(", ", this) + " ]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null) || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            ArrList<T> o = (ArrList<T>)obj;
+
+            if (items.Length != o.items.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!items[i].Equals(o.items[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 47;
+            int hash = 1;
+
+            foreach (var e in this)
+            {
+                hash = prime * hash + e.GetHashCode();
+            }
+
+            return hash;
+        }
+    }
+}
---
+++ ArrList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ArrList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrList<int> arrList1 = new ArrList<int>();
+
+            int[] array = new int[30];
+
+            for (int i = 1; i <= 20; i++)
+            {
+                arrList1.Add(i);
+            }
+
+            Console.WriteLine("Лист после инициализации:");
+            Console.WriteLine(arrList1);
+            Console.WriteLine();
+
+            arrList1.RemoveAt(5);
+            Console.WriteLine("Удаляем 5й элемент:");
+            Console.WriteLine(arrList1);
+            Console.WriteLine();
+
+            arrList1.Remove(5);
+            Console.WriteLine("Удаляем элемент с значениием 5:");
+            Console.WriteLine(arrList1);
+            Console.WriteLine();
+
+            Console.WriteLine("Есть элемент с значениием 5? {0}", arrList1.Contains(5));
+            Console.WriteLine();
+
+            Console.WriteLine("Индекс элемента со значениием 10 = {0}", arrList1.IndexOf(10));
+            Console.WriteLine();
+
+            arrList1.CopyTo(array, 6);
+            Console.WriteLine("Копирование в массив из 30 элементов:");
+            Console.WriteLine("[ " + string.Join(", ", array) + " ]");
+            Console.WriteLine();
+
+            arrList1.Insert(0, 20);
+            Console.WriteLine("Вставка числа 20 в начало:");
+            Console.WriteLine(arrList1);
+            Console.WriteLine();
+
+            ArrList<int> arrList2 = new ArrList<int>() { 1, 2, 3 };
+            ArrList<int> arrList3 = new ArrList<int>() { 1, 2, 3 };
+
+            Console.WriteLine("arrList2 эквивалентен arrList1? {0}", arrList2.Equals(arrList1));
+            Console.WriteLine("arrList2 эквивалентен arrList3? {0}", arrList2.Equals(arrList3));
+            Console.WriteLine();
+
+            Console.WriteLine("Хэш для arrList1 = {0}", arrList1.GetHashCode());
+            Console.WriteLine("Хэш для arrList2 = {0}", arrList2.GetHashCode());
+            Console.WriteLine("Хэш для arrList3 = {0}", arrList3.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
